42b7bc71cff4adba17e8684e33831efc0b6b7c11
mdk-man <73103726+mdk-man@users.noreply.github.com>
2020-10-19 17:18:15 +0200

    Update afrender_create.cmd
    make a correction on the double quote in the var declaration
    remove quotes in the "sc" command as they are embeded in the var ;)

--- afanasy/service/afrender_create.cmd
+++ afanasy/service/afrender_create.cmd
@@ -2,14 +2,14 @@ cd "%~dp0"
 
 call "setup.cmd"
 
-set "afservice=%AF_ROOT%\service\afservice.exe render"
+set afservice="%AF_ROOT%\service\afservice.exe render"
 
 if defined AF_SRV_USER if defined AF_SRV_PASS (
 	echo AF_SRV_USER=%AF_SRV_USER%
 	echo AF_SRV_PASS=%AF_SRV_PASS%
-	sc create afservice_render binpath= "%afservice%" type= own start= auto obj= %AF_SRV_USER% password= %AF_SRV_PASS%
+	sc create afservice_render binpath= %afservice% type= own start= auto obj= %AF_SRV_USER% password= %AF_SRV_PASS%
 ) else (
-	sc create afservice_render binpath= "%afservice%" type= own start= auto
+	sc create afservice_render binpath= %afservice% type= own start= auto
 )
 
 pause
